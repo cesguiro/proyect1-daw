014cf694a164126ddd6648ef1effa8ffd9a6fe12
César Guijarro <cesar.rosaleny@gmail.com>
2024-01-18 13:21:32 +0100

    Creación proyecto

---
+++ src/test/java/es/cesguiro/proyect1daw/Proyect1dawApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cesguiro.proyect1daw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyect1dawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
